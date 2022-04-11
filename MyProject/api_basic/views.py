21d0c73f16f8854b35f5b2e019dfa0d5c4ea5041
mrserjo <mrserjo@mail.ru>
2022-04-11 09:14:41 +0300

    hw

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -7,6 +7,34 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
+
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetails(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(pk=pk)
+
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
 
 
 @api_view(['GET', 'POST'])
